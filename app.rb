eb05b5bfc81bbc6579e9cee5154305989c8d7fe2
Oguz Huner <oguzcanhuner@gmail.com>
2013-12-09 18:30:05 +0000

    modularize into services

--- app.rb
+++ app.rb
@@ -4,51 +4,14 @@ require "haml"
 require "pry"
 
 require_relative "models/init"
+require_relative "services/init"
 
 class ReviewMyCode < Sinatra::Base
-  CLIENT_ID = '0e55acd10843bc0086b5'
-  CLIENT_SECRET = 'd362cf4dbb02fab923d1d5a737d786af6519e745'
-
-	enable :sessions
-
-  set :github_options, {
-    scopes:       "user",
-    secret:       CLIENT_SECRET,
-    client_id:    CLIENT_ID
-  }
-
-  register Sinatra::Auth::Github
-
-  def current_user
-    if authenticated? 
-      user = User.find_by_uuid(github_user.id)
-      if user.nil?
-        access_token = github_user["token"]
-        auth_result = RestClient.get("https://api.github.com/user", 
-                                     {:params => {access_token: access_token, accept: :json}, 
-                                                  accept: :json})
-        auth_result = JSON.parse(auth_result)
-        return User.create(uuid: auth_result['id'], gravatar_url: auth_result["avatar_url"])
-      else
-        return user
-      end
-    end
-  end
-
-  before do
-    @current_user = current_user
-  end
+  register API::Github
 
   get '/' do
-    if current_user
-      #gists_result = RestClient.get("https://api.github.com/gists", params: {user_id: 6975991}) 
-      #gists_result = JSON.parse(gists_result)
-    end
+    #gists_result = RestClient.get("https://api.github.com/gists", params: {user_id: 6975991}) 
+    #gists_result = JSON.parse(gists_result)
     haml :index
-  end
-
-  get '/login' do
-    authenticate!
-    redirect "/"
   end
 end
