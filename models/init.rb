ca261704136e3dbe5a305f14d9d5382093632e60
Oguz Huner <oguzcanhuner@gmail.com>
2013-12-12 13:20:49 +0000

    add mongohq url to models/init

--- models/init.rb
+++ models/init.rb
@@ -2,6 +2,6 @@ require "mongo_mapper"
 require_relative "user"
 require_relative "submission"
 
-MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
+MongoMapper.setup({'production' => {'uri' => ENV['MONGOHQ_URL']}}, 'production')
 MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 MongoMapper.database = "reviewmycode"
